54961b7193ae0664dcee113bf43642764b23d708
Alex Chebotarev <chebotarev.a@granit-concern.ru>
2021-09-03 14:17:37 +0300

    comments & exercises

--- parentheses/src/main.rs
+++ parentheses/src/main.rs
@@ -1,7 +1,7 @@
 pub fn is_valid(s: String) -> bool {
-    // let opened: Vec<char> = vec![];        
-    // let opened = Vec::<char>::new();        
-
+    // Упражнение 1. Распакуйте строку в вектор char'ов и тогда можно обращаться к отдельным буквам по индексу (по жизни так поступать не надо!)
+    //  Третий вариант -- работайте с байтами строки (s.bytes() или s.as_bytes(), а литерал будет выглядеть как b'(')
+    //  Закодируйте работу со строкой как с байтами. Придумайте тест, показывающий, что этот вариант работает неправильно.
     // let s2: Vec<char> = s.as_str().chars().collect();
     // for i in 0..s2.len() {
     //     if s[i] == '(' {
@@ -10,10 +10,17 @@ pub fn is_valid(s: String) -> bool {
     // }
 
     let mut opened: Vec<char> = Vec::new();        
+    // Альтернативные способы создать пустой вектор:
+    // let opened: Vec<char> = vec![];        
+    // let opened = Vec::<char>::new();        
+
     for c in s.as_str().chars() {
+        // Упражнение 2. Замените эти if'ы на match. От цикла по сортам скобок придётся отказаться
         if c == '(' || c == '[' || c == '{' {
             opened.push(c);
         } else {
+            // Тут у нас возникла проблема, что итерация по массиву, дающая (char, char) работает только в самых новых версиях раста.
+            //  В старых версиях доступна только итерация, дающая &(char, char), этот вариант вы видите ниже
             for &(open, close) in [('(', ')'), ('[', ']'), ('{', '}')].into_iter() {
                 if c == close {
                     let last = opened.pop();
@@ -23,27 +30,6 @@ pub fn is_valid(s: String) -> bool {
                 }
             }
         }
-
-        // ! match
-
-        // if c == ')' {
-        //     let last = opened.pop();
-        //     if last != Some('(') {
-        //         return false;
-        //     }
-        // }
-        // if c == ']' {
-        //     let last = opened.pop();
-        //     if last != Some('[') {
-        //         return false;
-        //     }
-        // }
-        // if c == '}' {
-        //     let last = opened.pop();
-        //     if last != Some('{') {
-        //         return false;
-        //     }
-        // }
     }
     opened.is_empty()
 }

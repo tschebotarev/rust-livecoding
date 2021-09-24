108680cf8dbc3a8fddac2eb4d38e5a1d954c450d
Alex Chebotarev <chebotarev.a@granit-concern.ru>
2021-09-24 19:21:04 +0300

    ref Vec

--- iterators1/src/main.rs
+++ iterators1/src/main.rs
@@ -1,23 +1,34 @@
 use rand::*;
-// use std::{thread, time::Duration};
+use std::{thread, time::Duration};
 
 
-const N: usize = 20;
-fn main() {
-    let mut rng = thread_rng();
-    let mut v = Vec::<u64>::new();
-    for _i in 0..N {
-        v.push(rng.gen_range(0..=1000));
-    }
-    println!("{:?}", v);
+const N: usize = 200_000_000;
 
+
+fn sum_up(v: &Vec<u64>) -> u64 {
     let mut s = 0;
     for i in 0..N {
         s += v[i]
     }
+    thread::sleep(Duration::from_millis(30000));
+    s
+}
+
+fn main() {
+    let mut rng = thread_rng();
+    // Генерация
+    let mut v = Vec::<u64>::new();
+    for _i in 0..N {
+        v.push(rng.gen_range(0..=1000));
+    }
+    // println!("{:?}", v);
+
+    // Подсчёт
+    let s = sum_up(&v);
+
     println!("{}", s);
 
-    // thread::sleep(Duration::from_millis(60000));
-    // println!("{}", v[N - 1]);
+    // thread::sleep(Duration::from_millis(30000));
+    println!("{}", v[N - 1]);
 
 }

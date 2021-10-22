b419267c978ca7f09bcef73a2587633c067bac1d
Alex Chebotarev <chebotarev.a@granit-concern.ru>
2021-10-22 21:20:28 +0300

    comments

--- priority/src/main.rs
+++ priority/src/main.rs
@@ -25,6 +25,7 @@ impl EndlessBoard {
     }
 
     fn generate(&mut self) -> i32 {
+        // Сложность поиска (и само существование) пути зависят от соотношения этих чисел (ещё с числом 100 далее в тексте)
         self.rng.gen_range(-11i32..5)
     }
 }
@@ -38,22 +39,42 @@ struct Arrival {
 struct PosWithPriority {
     total: i32,
     from: char,
+    // Для позиции, наверное, стоило выделить структуру
     pos: (u32, u32),
 }
 
+// Тащить 3 мутабельных объекта по ссылке -- это, конечно, уродливо.
+// Может быть лучше не выделять эту функцию, а всё это вставить в main ?
 fn find_way(
     board: &mut EndlessBoard,
     arrivals: &mut HashMap<(u32, u32), Arrival>,
     queue: &mut BinaryHeap<PosWithPriority>,
-    cur_pos: PosWithPriority
+    cur_pos: PosWithPriority,
 ) -> i32 {
-    if arrivals.entry(cur_pos.pos).or_insert(Arrival { total: cur_pos.total, from: cur_pos.from }).total > cur_pos.total {
+    if arrivals
+        .entry(cur_pos.pos)
+        .or_insert(Arrival {
+            total: cur_pos.total,
+            from: cur_pos.from,
+        })
+        .total
+        > cur_pos.total
+    {
         return i32::MIN;
     }
 
     let total = cur_pos.total + board.get(cur_pos.pos);
-    queue.push(PosWithPriority { pos: (cur_pos.pos.0 + 1, cur_pos.pos.1), from: 'x', total});
-    queue.push(PosWithPriority { pos: (cur_pos.pos.0, cur_pos.pos.1 + 1), from: 'y', total});
+    // Не класть в очередь, если total меньше сколько-то? Непонятно, как узнать порог
+    queue.push(PosWithPriority {
+        pos: (cur_pos.pos.0 + 1, cur_pos.pos.1),
+        from: 'x',
+        total,
+    });
+    queue.push(PosWithPriority {
+        pos: (cur_pos.pos.0, cur_pos.pos.1 + 1),
+        from: 'y',
+        total,
+    });
     total
 }
 
@@ -63,18 +84,25 @@ fn main() {
     let mut arrivals: HashMap<(u32, u32), Arrival> = HashMap::new();
     let mut queue: BinaryHeap<PosWithPriority> = BinaryHeap::new();
 
-    queue.push(PosWithPriority { pos: (0, 0), from: '?', total: 0});
+    queue.push(PosWithPriority {
+        pos: (0, 0),
+        from: '?',
+        total: 0,
+    });
     let mut res: (u32, u32);
     loop {
         let next_pos = queue.pop().unwrap();
-        if find_way(&mut board, &mut arrivals, &mut queue, next_pos.clone()) >= 100 {
+        if find_way(&mut board, &mut arrivals, &mut queue, next_pos) >= 100 {
             res = next_pos.pos;
             break;
         }
+        // Проверить размер очереди и устроить ей подрезание??
     }
 
-    // let mut pos = find_way(&mut board, &mut arrivals, &mut queue, (0, 0), 0);
     let mut path: Vec<((u32, u32), i32)> = vec![];
+    // Можно ли переписать, используя итераторы, так чтобы
+    //  1) избавиться от mut res
+    //  2) чтобы вместо push было collect в конце
     while res != (0, 0) {
         let next_res = if arrivals.get(&res).unwrap().from == 'x' {
             (res.0 - 1, res.1)

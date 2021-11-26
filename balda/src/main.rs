7bd44e3380f5ac6b3a90fa652628aa22d91c8c99
Alex Chebotarev <chebotarev.a@granit-concern.ru>
2021-11-26 20:44:39 +0300

    sample of threads and global vars

--- balda/src/main.rs
+++ balda/src/main.rs
@@ -1,21 +1,27 @@
+use dashmap::DashSet;
+use parking_lot::RwLock;
 use std::collections::btree_set::Range;
 use std::collections::{BTreeSet, HashSet};
 use std::fs::File;
 use std::io::{self, stdin, BufRead};
 use std::path::Path;
+use std::thread::spawn;
+use std::time::Duration;
 
-fn get_all_by_prefix(dict: &BTreeSet<String>, prefix: &str) -> Vec<String> {
+fn get_all_by_prefix(dict: &DashSet<String>, prefix: &str) -> Vec<String> {
     // WHERE S like 'prefix%'
     // WHERE S >= 'prefix' ans S < 'prefixяяяяяяяяяяя'
     //
-    let next = format!("{}я", prefix);
-    let range: Range<String> = dict.range(prefix.to_string()..=next);
+    // let next = format!("{}я", prefix);
+    // let range: Range<String> = dict.range(prefix.to_string()..=next);
     // (0..10)
     // (Included(0), Excluded(10))
 
-    range.into_iter().cloned().collect()
+    // range.into_iter().cloned().collect()
+    vec![]
 }
 
+#[derive(PartialEq)]
 enum PositionEstimation {
     BadPosition,
     AlreadyWin,
@@ -26,12 +32,15 @@ enum PositionEstimation {
     CanWin(String, usize),
 }
 
-fn is_winner(dict: &BTreeSet<String>, prefix: &str) -> PositionEstimation {
+fn is_winner(dict: &DashSet<String>, prefix: &str) -> PositionEstimation {
     let layer = get_all_by_prefix(dict, prefix);
     if layer.is_empty() {
         return PositionEstimation::BadPosition;
     }
     if layer.iter().any(|s| s.as_str() == prefix) {
+        return PositionEstimation::AlreadyWin;
+    }
+    if *FOUND.read() {
         return PositionEstimation::AlreadyWin;
     }
 
@@ -48,6 +57,7 @@ fn is_winner(dict: &BTreeSet<String>, prefix: &str) -> PositionEstimation {
                 return PositionEstimation::CanWin(my_move, n + 1);
             }
             PositionEstimation::AlreadyLost(_) => {
+                *FOUND.write() = true;
                 return PositionEstimation::CanWin(my_move, 1);
             }
             PositionEstimation::CanWin(_, n) => {
@@ -66,6 +76,53 @@ fn is_winner(dict: &BTreeSet<String>, prefix: &str) -> PositionEstimation {
     }
 }
 
+fn is_winner_first_move(prefix: &str) -> PositionEstimation {
+    const THREAD_COUNT: usize = 4;
+    let next_move: Vec<String> = "abcdefghijklmnopqrstuvwxyz"
+        .chars()
+        .map(|c| format!("{}{}", prefix, c))
+        .collect();
+
+    let parts: Vec<Vec<String>> = next_move
+        .chunks(next_move.len() / THREAD_COUNT + 1)
+        .map(|x| x.to_vec())
+        .collect();
+
+    let handles: Vec<_> = parts
+        .into_iter()
+        .map(|part| {
+            // let dict = dict.clone();
+            spawn(move || -> bool {
+                for s in &part {
+                    if let PositionEstimation::CannotWin(_, _) = is_winner(&DICT, s) {
+                        return true;
+                    }
+                }
+                false
+            })
+        })
+        .collect();
+
+    let mut victory = false;
+    for handle in handles {
+        if handle.join().unwrap() {
+            victory = true;
+        }
+    }
+
+    if victory {
+        PositionEstimation::AlreadyWin
+    } else {
+        PositionEstimation::CannotWin("((".to_string(), 0)
+    }
+}
+
+use lazy_static::lazy_static;
+lazy_static! {
+    static ref DICT: DashSet<String> = DashSet::new();
+    static ref FOUND: RwLock<bool> = RwLock::new(false);
+}
+
 fn main() {
     let path = Path::new("../wordlist.10000.txt");
     let file = File::open(path).unwrap_or_else(|err| {
@@ -75,9 +132,12 @@ fn main() {
     println!("{}", v.len());
 
     let v: Vec<String> = v.into_iter().filter_map(|x| x.ok()).collect();
-    let m: BTreeSet<String> = v.into_iter().filter(|s| s.len() > 1).collect();
-
-    is_winner(&m, "bhuta");
+    {
+        for s in v.into_iter().filter(|s| s.len() > 1) {
+            DICT.insert(s);
+        }
+    }
+    // let m: BTreeSet<String> = v.into_iter().filter(|s| s.len() > 1).collect();
 
     let mut current: String = "".to_string();
     loop {
@@ -85,11 +145,12 @@ fn main() {
         stdin().lock().read_line(&mut input).unwrap();
         input = input.trim().to_string();
         if input == "?" {
-            println!("{:?}", get_all_by_prefix(&m, &current));
+            println!("{:?}", get_all_by_prefix(&DICT, &current));
         } else if input.len() != current.len() + 1 || input[0..current.len()] != current {
             println!("Cheater!!");
         } else {
-            match is_winner(&m, &input) {
+            *FOUND.write() = false;
+            match is_winner_first_move(&input) {
                 PositionEstimation::CanWin(m, _) | PositionEstimation::CannotWin(m, _) => {
                     println!(">{}", m);
                     current = m;

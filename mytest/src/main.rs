1a1053bb2e37f49dc51c6708d98ac84f7265ef04
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2022-02-04 11:55:38 +0300

    have errors.....in universal type

--- mytest/src/main.rs
+++ mytest/src/main.rs
@@ -1,3 +1,4 @@
+use num::Integer; // num = "0.2.1"
 use std::mem::size_of;
 
 // radix-sort
@@ -10,22 +11,34 @@ https://leetcode.com/problems/interleaving-string/
 https://leetcode.com/problems/maximal-rectangle/
 */
 
-fn ultra_sort(array:&mut Vec<i32>) { 
-    let steps = size_of::<i32>()*2;
+fn fn_for_sort<T>(n:T, a:usize, steps:usize) -> usize 
+where T: num::Integer,
+{
+    // ((array_save[i]>>(4*a))&15) as usize; // start
+    let number = ((n >> ((4*a))&15) ) as usize;
+    
+    // для отрицательных чисел
+    if a==steps-1 {
+        // number ^= 8;
+        return number^8;
+    }
+    number
+}
+
+fn ultra_sort<T>(array:&mut Vec<T>) 
+where T: num::Integer + Clone, //  + Default
+{ 
+    let steps = size_of::<T>()*2;
     //println!("{}",steps);
     for a in 0..steps {
         let array_save = array.clone();
         //let mut test:Vec<u32> = vec![0;10];
         let mut test:Vec<u32> = vec![0;16];
         for i in 0..array.len() {
-            //let number = ((array_save[i]%(10_u32.pow(a)))/(10_u32.pow(a-1))) as usize;
-            let mut number = ((array_save[i]>>(4*a))&15) as usize;
-            if a==steps-1 {
-                number ^= 8;
-            }
+            let mut number = fn_for_sort(array_save[i],a,steps);
             //println!("{} {} {}",i,array_save[i],number);
             test[number] += 1;
-            array[i] = 0;
+            array[i] = 0 as T;
         }
         //println!("{:?}",test);
         //let mut place:Vec<u32> = vec![0;10];
@@ -36,11 +49,7 @@ fn ultra_sort(array:&mut Vec<i32>) {
         }
         //println!("{:?}",place);
         for i in 0..array.len() {
-            //let number = ((array_save[i]%(10_u32.pow(a)))/(10_u32.pow(a-1))) as usize;
-            let mut number = ((array_save[i]>>(4*a))&15) as usize;
-            if a==steps-1 {
-                number ^= 8;
-            }
+            let mut number = fn_for_sort(array_save[i],a,steps);
             //println!("{} {} {}",i,array_save[i],number);
             array[place[number] as usize] = array_save[i];
             place[number] += 1;
@@ -49,7 +58,7 @@ fn ultra_sort(array:&mut Vec<i32>) {
 }
 
 fn main() {
-    let mut array = vec![12,23,24,17,-36,56,32,11];
+    let mut array:Vec<i32> = vec![12,23,24,17,-36,56,32,11];
     println!("basic:  {:?}", array);
     ultra_sort(&mut array);
     println!("answer: {:?}", array);

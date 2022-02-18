74181399ad1108f7318bb850b618816cdef6900a
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2022-02-18 20:33:12 +0300

    lesson

--- lessons/lib_2022_02_18.rs
+++ lessons/lib_2022_02_18.rs
@@ -1,3 +1,23 @@
+// cat_dog
+
+/*
+// main
+
+mod lib;
+use lib::{Database,Animal::*};
+
+fn main() {
+    let mut shelter = Database::default();
+    shelter.push(Cat("Barsik"));
+    shelter.push(Cat("Mursik"));
+    shelter.push(Dog("Mukhtar"));
+    println!("pop: {}",shelter.pop().unwrap());
+    println!("cat: {}",shelter.pop_cat().unwrap());
+    println!("dog: {}",shelter.pop_dog().unwrap());
+    shelter.clear();
+}
+*/
+
 #[derive(Clone, Copy, Hash, PartialEq, Eq)]
 pub enum Animal {
     Dog(&'static str),

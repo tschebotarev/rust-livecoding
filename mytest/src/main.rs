54d8e1f266fa0007c75b7badf4f30dd903a56d81
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2022-03-07 21:25:22 +0300

    Update main.rs

--- mytest/src/main.rs
+++ mytest/src/main.rs
@@ -1,6 +1,9 @@
 use std::ops::{Add, Sub};
+use rand::Rng;
 
-const PAGE_SIZE:usize = 8196;
+//const PAGE_SIZE:usize = 8196;
+const PAGE_SIZE:usize = 50;
+
 
 #[derive(Clone, Copy)]
 struct BackwardOffset(u16);
@@ -8,6 +11,13 @@ struct BackwardOffset(u16);
 #[derive(Clone, Copy)]
 struct Length(u16);
 
+
+// impl From<BackwardOffset> for usize {
+//     fn from(item: BackwardOffset) -> Self {
+//         let dist: usize = item.0.into();
+//         PAGE_SIZE - dist
+//     }
+// }
 
 impl From<BackwardOffset> for usize {
     fn from(item: BackwardOffset) -> Self {
@@ -39,39 +49,90 @@ impl Add<Length> for BackwardOffset {
 //     }
 // }
 
-// impl Sub<BackwardOffset> for usize {
-//     type Output = usize;
-//     fn sub(self, rhs: BackwardOffset) -> Self::Output {
-//         self - rhs.0 as usize
-//     }
+impl Sub<BackwardOffset> for BackwardOffset {
+    type Output = usize;
+    fn sub(self, rhs: BackwardOffset) -> Self::Output {
+        (self.0 - rhs.0) as usize 
+    }
 
-// }
+}
 
 
-fn move_row(
-    buf: &mut [u8; PAGE_SIZE],
-    old_pos: BackwardOffset,
-    new_pos: BackwardOffset,
-    len: Length,
-) {
-    let old_offset: usize = old_pos.into();
+#[derive(Clone)]
+struct DataBase {
+    //array: [u8; PAGE_SIZE],
+    array: Vec<u8>,
+}
 
-    let next = old_offset + len;
-    let next_pos = old_pos + len;
+impl Default for DataBase {
+    fn default() -> Self {
+        let array:Vec<u8> = vec![0;PAGE_SIZE];
+        Self {
+            array,
+        }
+    }
+}
 
-    //let x = new_pos + old_pos;
+impl DataBase {
+    pub fn push(&mut self, add: u8) {
+        self.array.push(add);
+    }
+    pub fn remove(&mut self, number: usize) {
+        self.array.remove(number);
+    }
+    pub fn get(&self, number: usize) -> u8 {
+        return self.array[number];
+    }
+    pub fn print(&self) {
+        println!("{:?}", self.array);
+    }
+    pub fn randoming(&mut self) {
+        let mut rng = rand::thread_rng();
+        for i in 0..self.array.len() {
+            self.array[i] = rng.gen_range(0..10);
+        }
+    }
+    pub fn move_row(&mut self, old_pos: BackwardOffset, new_pos: BackwardOffset, len: Length) {
+        // (
+        //     &mut self, 
+        //     //buf: &mut [u8; PAGE_SIZE],
+        //     //buf: &mut DataBase,
+        //     old_pos: BackwardOffset,
+        //     new_pos: BackwardOffset,
+        //     len: Length,
+        // )
 
-    // timestamp -- секунды от рождества христова
-    // interval -- секунды со знаком
+        
+        let old_offset:usize = old_pos.into();
+    
+        let next = old_offset + len;
+        let next_pos = new_pos - old_pos;
 
-    let new_offset: usize = PAGE_SIZE - new_pos;
-    let len: usize = len.0.into();
-    // let len: usize = len.0 as usize;
-
-    //buf[new_offset..new_offset + len].copy_from_slice(&buf[old_offset..old_offset + len]);
-
-} 
+        for i in old_offset..next {
+            let t = self.array[i+next_pos];
+            self.array[i+next_pos] = self.array[i];
+            self.array[i] = t;
+        }
+    } 
+}
 
 fn main() {
+    print!("create array:                ");
+    let mut data = DataBase::default();
+    data.print();
+    print!("write random data in array:  ");
+    data.randoming();
+    data.print();
+    print!("add new element array:       ");
+    data.push(23);
+    data.print();
+    print!("get last element array:      ");
+    println!("{}",data.get(PAGE_SIZE));
+    print!("del last element array:      ");
+    data.remove(PAGE_SIZE);
+    data.print();
+    print!("move data in array:          ");
+    data.move_row(BackwardOffset(3), BackwardOffset(4), Length(2));
+    data.print();
 
 }
